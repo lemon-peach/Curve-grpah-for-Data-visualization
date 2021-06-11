69403f19923a820871ac622c3c858930c17498d8
lemon-peach <56794514+lemon-peach@users.noreply.github.com>
2021-06-11 16:03:28 +0800

    Add files via upload

--- get_parameter.py
+++ get_parameter.py
@@ -147,88 +147,88 @@ except configparser.MissingSectionHeaderError:
     config.read(config_path, encoding='utf-8-sig')
 
 # [ico]
-ico_type = config.get('ico', 'ico type', fallback='marker')
-marker_style = ini.get_param_from_str(config.get('ico', 'marker style', fallback='.'))
+ico_type = config.get('ico', 'ico_type', fallback='marker')
+marker_style = ini.get_param_from_str(config.get('ico', 'marker_style', fallback='.'))
 ico_path_dict = dict(config.items('ico'))
-ico_zoom = ini.get_float('ico', 'ico zoom', fallback='1')
+ico_zoom = ini.get_float('ico', 'ico_zoom', fallback='1')
 
 # [figure]
 # ----------------figure, axes---------------------
-fig_size = ini.get_list('figure', 'fig size', fallback='(10, 5)')
+fig_size = ini.get_list('figure', 'fig_size', fallback='(10, 5)')
 if len(fig_size) != 2:
     fig_size = (10, 5)
 if fig_size[0] > 15:
     fig_size = (15, fig_size[1])
 if fig_size[1] > 8:
     fig_size = (fig_size[0], 8)
-axes_rect = ini.get_list('figure', 'axes rect', fallback='(0.1, 0.1, 0.8, 0.8)')
+axes_rect = ini.get_list('figure', 'axes_rect', fallback='(0.1, 0.1, 0.8, 0.8)')
 if len(axes_rect) != 4:
     axes_rect = (0.1, 0.1, 0.8, 0.8)
-axes_frame_on = ini.get_bool('figure', 'axes frame on', fallback='False')
-right_space = ini.get_float('figure', 'right space', fallback='1.5')
+axes_frame_on = ini.get_bool('figure', 'axes_frame on', fallback='False')
+right_space = ini.get_float('figure', 'right_space', fallback='1.5')
 
 # -----------------------line-------------------------
-auto_fill = ini.get_bool('figure', 'auto fill', fallback='True')
-bezier_smoothness = ini.get_int('figure', 'bezier smoothness', fallback='10')
-bezier_auto_rotate = ini.get_bool('figure', 'bezier auto rotate', fallback='True')
-bezier_auto_scale = ini.get_bool('figure', 'bezier auto scale', fallback='True')
-bezier_scale_range = ini.get_list('figure', 'bezier scale range', fallback='(0.5, 0.9)')
+auto_fill = ini.get_bool('figure', 'auto_fill', fallback='True')
+bezier_smoothness = ini.get_int('figure', 'bezier_smoothness', fallback='10')
+bezier_auto_rotate = ini.get_bool('figure', 'bezier_auto_rotate', fallback='True')
+bezier_auto_scale = ini.get_bool('figure', 'bezier_auto_scale', fallback='True')
+bezier_scale_range = ini.get_list('figure', 'bezier_scale_range', fallback='(0.5, 0.9)')
 if len(bezier_scale_range) != 2:
     bezier_scale_range = (0.5, 0.9)
-point_div = 1 / ini.get_int('figure', 'point div', fallback='20')
+point_div = 1 / ini.get_int('figure', 'point_div', fallback='20')
 
 # -----------------------grid-------------------------
-y_grid_on = ini.get_bool('figure', 'y grid on', fallback='False')
-x_grid_on = ini.get_bool('figure', 'x grid on', fallback='False')
-y_grid_alpha = ini.get_float('figure', 'y grid alpha', fallback='0.2')
-x_grid_alpha = ini.get_float('figure', 'y grid alpha', fallback='0.2')
-y_grid_linestyle = ini.get_param_from_str(config.get('figure', 'y grid linestyle', fallback='--'))
+y_grid_on = ini.get_bool('figure', 'y_grid_on', fallback='False')
+x_grid_on = ini.get_bool('figure', 'x_grid_on', fallback='False')
+y_grid_alpha = ini.get_float('figure', 'y_grid_alpha', fallback='0.2')
+x_grid_alpha = ini.get_float('figure', 'y_grid_alpha', fallback='0.2')
+y_grid_linestyle = ini.get_param_from_str(config.get('figure', 'y_grid_linestyle', fallback='--'))
 if isinstance(y_grid_linestyle, list):
     y_grid_linestyle = tuple(y_grid_linestyle)
-x_grid_linestyle = ini.get_param_from_str(config.get('figure', 'x grid linestyle', fallback='--'))
+x_grid_linestyle = ini.get_param_from_str(config.get('figure', 'x_grid_linestyle', fallback='--'))
 if isinstance(x_grid_linestyle, list):
     x_grid_linestyle = tuple(x_grid_linestyle)
 
 # ----------------------axis---------------------
-xlabel_prefix = config.get('figure', 'x label prefix', fallback='')
-xlabel_suffix = config.get('figure', 'x label suffix', fallback='')
-xlabel_rotation = ini.get_float('figure', 'x label rotation', fallback='0')
-major_xlabel = config.get('figure', 'major x label', fallback='A')
+xlabel_prefix = config.get('figure', 'x_label_prefix', fallback='')
+xlabel_suffix = config.get('figure', 'x_label_suffix', fallback='')
+xlabel_rotation = ini.get_float('figure', 'x_label_rotation', fallback='0')
+major_xlabel = config.get('figure', 'major_x_label', fallback='A')
 major_xlabel_code = date_code_dict.get(major_xlabel, 0)
-max_xsection_stage2 = ini.get_int('figure', 'max x section stage2', fallback='-1')
+max_xsection_stage2 = ini.get_int('figure', 'max_x_section_stage2', fallback='-1')
 if max_xsection_stage2 == -1:
     max_xsection_stage2 = int(fig_size[0] / 2)
-max_xsection_stage3 = ini.get_int('figure', 'max x section stage3', fallback='-1')
+max_xsection_stage3 = ini.get_int('figure', 'max_x_section_stage3', fallback='-1')
 if max_xsection_stage3 == -1:
     max_xsection_stage3 = int(fig_size[0] / 1.5)
 
-y_lim = ini.get_list('figure', 'y lim', fallback='(0, 0)')
-y_label_format = config.get('figure', 'y label format', fallback='0')
-ylabel_prefix = config.get('figure', 'y prefix', fallback='')
-ylabel_suffix = config.get('figure', 'y suffix', fallback='')
+y_lim = ini.get_list('figure', 'y_lim', fallback='(0, 0)')
+y_label_format = config.get('figure', 'y_label_format', fallback='0')
+ylabel_prefix = config.get('figure', 'y_prefix', fallback='')
+ylabel_suffix = config.get('figure', 'y_suffix', fallback='')
 
 # -------------------Annotation-------------------
-x_unit = config.get('figure', 'x unit', fallback='')
-x_unit_position = ini.get_list('figure', 'x unit position', fallback='(-1.0, -1.0)')
+x_unit = config.get('figure', 'x_unit', fallback='')
+x_unit_position = ini.get_list('figure', 'x_unit_position', fallback='(-1.0, -1.0)')
 if len(x_unit_position) != 2:
     x_unit_position = (-1.0, -1.0)
-y_unit = config.get('figure', 'y unit', fallback='')
-y_unit_position = ini.get_list('figure', 'y unit position', fallback='(-1.0, -1.0)')
+y_unit = config.get('figure', 'y_unit', fallback='')
+y_unit_position = ini.get_list('figure', 'y_unit_position', fallback='(-1.0, -1.0)')
 if len(y_unit_position) != 2:
     y_unit_position = (-1.0, -1.0)
-legend_on = ini.get_bool('figure', 'legend on', fallback='False')
-legend_color = ini.get_bool('figure', 'legend color', fallback='False')
+legend_on = ini.get_bool('figure', 'legend_on', fallback='False')
+legend_color = ini.get_bool('figure', 'legend_color', fallback='False')
 
 # [line color]
 line_color_dict = dict()
-for key, value in config.items('line color'):
+for key, value in config.items('line_color'):
     line_color_dict[key] = ini.get_param_from_str(value)
 
 # [video]
-frame_div = 1 / ini.get_int('video', 'frame div', fallback='20')
+frame_div = 1 / ini.get_int('video', 'frame_div', fallback='20')
 video_fps = ini.get_int('video', 'fps', fallback='30')
-video_type = config.get('video', 'video type', fallback='avi')
-ffmpeg_path = config.get('video', 'ffmpeg path', fallback='')
+video_type = config.get('video', 'video_type', fallback='avi')
+ffmpeg_path = config.get('video', 'ffmpeg_path', fallback='')
 synchro = ini.get_bool('video', 'synchro', fallback='False')
 video_output_path = os.path.join(os.getcwd(), 'files/statistics.avi')
 
@@ -238,7 +238,7 @@ for key, value in config.items('rcParams'):
     rcParams_dict[key] = value if value[0] != '\\' else value[1:]
 
 # [axes setattr]
-_axes_setattr = dict(config.items('axes setattr'))
+_axes_setattr = dict(config.items('axes_setattr'))
 axes_setattr = dict()
 for key, value in _axes_setattr.items():
     args = []
